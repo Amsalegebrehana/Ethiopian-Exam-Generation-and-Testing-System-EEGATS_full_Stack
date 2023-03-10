71cea1cf6bf32620f1d4b4ad395c0b68d6df486f
Selamawit926 <se.selamawit.elias@gmail.com>
2023-03-10 14:18:31 +0300

    Exams page added

---
+++ pages/admin/exams/[id].vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <AdminTopBar role="admin" />
+        <div class="flex">
+            
+            <AdminSideBar pageName="exams" />
+            <div class="w-full mx-6">
+                
+                <h1>ExamId: {{ $route.params.id }}</h1>
+                  <h2>Exam detail page</h2>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+definePageMeta({ middleware: 'is-admin' })
+import AdminTopBar from '~~/components/TopBar.vue'
+import AdminSideBar from '~~/components/admin/AdminSideBar.vue';
+export default {
+    components: { AdminSideBar, AdminTopBar },
+    name: 'ExamDeatil',
+    data() {
+        return {
+            }
+
+    },
+}
+</script>
