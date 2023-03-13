cabea2d62ddf90a77e3ed8f99de89e3409bd37e3
Selamawit926 <se.selamawit.elias@gmail.com>
2023-03-13 22:55:43 +0300

    examGroup router added

--- server/trpc/routers/index.ts
+++ server/trpc/routers/index.ts
@@ -1,11 +1,14 @@
 import { z } from 'zod'
 import { publicProcedure, router } from '../trpc'
 
+import { examGroupRouter } from './examGroups';
 import { poolRouter } from './pool';
+
 export const appRouter = router({
   
     pool: poolRouter,
-    
+    examGroup: examGroupRouter
+
   
 });
 
