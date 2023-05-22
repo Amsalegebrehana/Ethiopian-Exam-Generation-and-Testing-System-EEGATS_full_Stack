4a3d184e71a44be0e7d39bbbcfe022ec48a00a5a
sitota49 <se.sitota.ezra@gmail.com>
2023-05-22 16:28:39 +0300

    loading icon on login and autocomplete off

--- pages/contributor/login.vue
+++ pages/contributor/login.vue
@@ -12,45 +12,55 @@
                 <img src="../../assets/images/undraw_Teacher_re_sico.png" class="mx-auto h-4/6  " />
             </div>
             <div class=" h-screen align-middle my-auto py-10">
-                    <form @submit="onSubmit">
-                <div class="w-8/12 mt-32 justify-center mx-auto">
-            <span class="mt-5 text-red-500">{{ formError }}</span>
-                    <div class="intro-x mt-8">
-         
-                    <span class="mt-5 text-red-500">{{ errors.email }}</span>
-                        <div class="input-group my-2  w-96">
-                            <div class="input-group-text">
-                                <Icon name="material-symbols:alternate-email" class="w-4 h-4"></Icon>
-                            </div>
-                            <input  type="text" name="email"  class="form-control bg-slate-100" placeholder="Email"  v-model="email">
-                        
-                        </div>
-               
-                   <span class="mt-10 text-red-500">{{ errors.password }}</span>
-                        <div class="input-group mt-2  w-96" >
-                            <div class="input-group-text"><Icon name="material-symbols:alternate-email" class="w-4 h-4"></Icon></div>
-                            <input :type="showPassword ? 'text' : 'password'" class="form-control bg-slate-100" placeholder="Password"  v-model="password" :class="{ 'has-error': errors.password != undefined }"   >
-                        
-                            <div v-on:click="togglePassword()" class="input-group-text">
-                                <Icon v-if="showPassword" name="ri:eye-line" class="w-4 h-4 text-slate-500"></Icon>
-                            <Icon v-else name="ri:eye-close-line" class="w-4 h-4 text-slate-500"></Icon>
-                        </div>
-                    
-                    </div>
-                
+                <form @submit="onSubmit">
+                    <div class="w-8/12 mt-32 justify-center mx-auto">
+                        <span class="mt-5 text-red-500">{{ formError }}</span>
+                        <div class="intro-x mt-8">
 
-                    </div>
-                    <!-- <div class="intro-x flex text-slate-600 dark:text-slate-500 text-xs sm:text-sm mt-4">
+                            <span class="mt-5 text-red-500">{{ errors.email }}</span>
+                            <div class="input-group my-2  w-96">
+                                <div class="input-group-text">
+                                    <Icon name="material-symbols:alternate-email" class="w-4 h-4"></Icon>
+                                </div>
+                                <input type="text" name="email" class="form-control bg-slate-100" placeholder="Email"
+                                    v-model="email" autocomplete="new-password">
+
+                            </div>
+
+                            <span class="mt-10 text-red-500">{{ errors.password }}</span>
+                            <div class="input-group mt-2  w-96">
+                                <div class="input-group-text">
+                                    <Icon name="material-symbols:alternate-email" class="w-4 h-4"></Icon>
+                                </div>
+                                <input :type="showPassword ? 'text' : 'password'" class="form-control bg-slate-100"
+                                    placeholder="Password" v-model="password"
+                                    :class="{ 'has-error': errors.password != undefined }" autocomplete="new-password">
+
+                                <div v-on:click="togglePassword()" class="input-group-text">
+                                    <Icon v-if="showPassword" name="ri:eye-line" class="w-4 h-4 text-slate-500"></Icon>
+                                    <Icon v-else name="ri:eye-close-line" class="w-4 h-4 text-slate-500"></Icon>
+                                </div>
+
+                            </div>
+
+
+                        </div>
+                        <!-- <div class="intro-x flex text-slate-600 dark:text-slate-500 text-xs sm:text-sm mt-4">
                     <div></div>
                     <a href="" class="ml-auto">Forgot Password?</a>
                 </div> -->
-         
-                
+
+
                         <button class="bg-primary rounded-xl w-5/12 text-white py-3 px-4 text-center mt-8">
-                            Sign In
+                            <div v-if="isLoading">
+                                <Icon name="eos-icons:bubble-loading" class="w-6 h-6"></Icon>
+                            </div>
+                            <div v-else>
+                                Sign In
+                            </div>
                         </button>
-              
-                </div>
+
+                    </div>
                 </form>
 
             </div>
@@ -66,7 +76,7 @@ import * as zod from 'zod';
 definePageMeta({ auth: false })
 
 const { $client } = useNuxtApp()
-
+const isLoading = ref(false);
 const showPassword = ref(false);
 const togglePassword = () => {
     showPassword.value = !showPassword.value;
@@ -94,15 +104,15 @@ const onSubmit = handleSubmit(values => {
 });
 const { signIn } = useSession()
 const mySignInHandler = async ({ email, password, role }) => {
-
-    const {data: contrId} = await useAsyncData( ()=> $client.contributor.getContributorId.query({email}));
-
+    isLoading.value = true;
+    const { data: contrId } = await useAsyncData(() => $client.contributor.getContributorId.query({ email }));
     const { error, url } = await signIn('credentials', { email, password, role, redirect: false, callbackUrl: `http://localhost:3000/contributor/${contrId._rawValue}/questions` })
     if (error) {
-         formError.value = "Incorrect credentials! Please try again";
-        } else {
+        formError.value = "Incorrect credentials! Please try again";
+    } else {
         return navigateTo(url, { external: true })
     }
+    isLoading.value = false;
 }
 
 
