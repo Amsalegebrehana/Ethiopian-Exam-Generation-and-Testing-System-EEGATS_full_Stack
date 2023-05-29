583c4fd7fbf4ad29b6a065c0decdf0cffb1aa747
Sitota Ezra Mersha <52606757+sitota49@users.noreply.github.com>
2023-05-29 15:23:58 +0300

    Analytics (#46)
    * failed attempts lockout and fixed login form
    
    * admin portal to reset password
    
    * protecting register route and error handling
    
    * test taker dashboard search, pagination , start exam error handling and test taker analytics
    
    * folder restructuring pools
    
    * Update [id].vue
    
    * Update testTaker.ts
    
    ---------
    
    Co-authored-by: Amsale Gebrehana <101251018+AmsaleG@users.noreply.github.com>

---
+++ pages/admin/contributors/index.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <div>
+        <AdminTopBar role="admin" />
+        <div class="flex">
+
+            <AdminSideBar pageName="testtakers" />
+            <div class="w-full mx-6">
+
+                <h1>Contributors</h1>
+            </div>
+            </div>
+            
+       
+    </div>
+</template>
+<script>
+</script>
---
+++ pages/admin/pools/[id]/assign-contributor.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const contrId = useRoute().query.contrId;
+const { $client } = useNuxtApp();
+</script>
+<template>
+    <h1>assign contributor</h1>
+    <p>{{ contrId }}</p>
+</template>
