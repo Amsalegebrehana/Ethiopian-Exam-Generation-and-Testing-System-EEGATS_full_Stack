548091d4afa0a8e8ea53c6b4e7d68c09e7dd991b
AmsaleG <amsale@kemercode.com>
2023-06-08 18:12:28 +0300

    cards changed

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -4,84 +4,128 @@
         <div class="flex">
 
             <AdminSideBar pageName="analytics" />
-            <div class="w-full mx-10">
-                <div v-if="analytics">
-                    <div class="container mx-auto py-8 align-middle justify-center items-center w-3/4 ">
-                        <div class="flex flex-row">
-                            <div class="w-4/12 p-6 align-middle">
-
-
-
-                                <div class="align-middle">
-                                    <a :href="`/admin/exams`">
-
-                                        <div class="bg-white rounded-lg border-1 shadow-lg p-10 mt-5 hover:bg-gray-50 ">
-
-                                            <div class="flex flex-row justify-between  items-center text-center">
-                                                <div class="text-left align-middle">
-                                                    <Icon name="healthicons:i-exam-qualification-outline"
-                                                        class="h-16 w-16  my-2 text-primary align-middle">
-                                                    </Icon>
-                                                </div>
-                                                <div class="text-right align-middle ">
-                                                    <h2 class="text-2xl font-bold mb-2 text-gray-600 text-center">Exams
-                                                    </h2>
-                                                    <p class="text-3xl font-bold text-gray-800">{{
-                                                        analytics.totalExams }}
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </a>
-
-                                    <a :href="`/admin/pools`">
-                                        <div class="bg-white rounded-lg border-1 shadow-lg p-10 mt-5 hover:bg-gray-50 ">
-
-                                            <div class="flex flex-row justify-between  items-center text-center">
-                                                <div class="text-left align-middle">
-                                                    <Icon name="solar:pie-chart-broken"
-                                                        class="h-16 w-16  my-2 text-primary align-middle">
-                                                    </Icon>
-                                                </div>
-                                                <div class="text-right align-middle ">
-                                                    <h2 class="text-2xl font-bold mb-2 text-gray-600 text-center">Pools
-                                                    </h2>
-                                                    <p class="text-3xl font-bold text-gray-800">{{
-                                                        analytics.totalPools }}
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                    </a>
-                                </div>
-                            </div>
-
-
-                            <div class="w-8/12">
-
-
-                                <a :href="`admin/contributors`" class="mt-4">
-                                    <div class="bg-white rounded-lg shadow-lg p-6 mt-2 hover:bg-gray-50 h-96">
-                                        <Doughnut :data="analytics.contributorDistribution.data"
-                                            :options="analytics.contributorDistribution.options" />
+            <div class="content">
+                <div class="grid grid-cols-12 gap-6">
+                    <div class="col-span-12 2xl:col-span-9">
+                    <div v-if="analytics"  class="grid grid-cols-12 gap-6">
+                     <!-- BEGIN: General Report -->
+                        <div class="col-span-12 mt-8">
+                                    <div class="intro-y flex items-center h-10">
+                                        <h2 class="text-lg font-medium truncate mr-5">
+                                            General Report
+                                        </h2>
+                                        <a href="" class="ml-auto flex items-center text-primary"> <i data-lucide="refresh-ccw" class="w-4 h-4 mr-3"></i> Reload Data </a>
                                     </div>
-                                </a>
-
-                            </div>
+                                    <div class="grid grid-cols-12 gap-6 mt-5">
+                                        <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+                                            <div class="report-box zoom-in">
+                                                <div class="box p-5">
+                                                    <div class="flex">
+                                                        <i data-lucide="shopping-cart" class="report-box__icon text-primary"></i> 
+                                                        <div class="ml-auto">
+                                                           
+                                                            <Icon name="solar:pie-chart-broken"
+                                                                class=" w-16 h-10  my-2 text-primary ">
+                                                            </Icon>
+                                                         </div>
+                                                    </div>
+                                                    <div class="text-3xl font-medium leading-8 mt-6">{{
+                                                                analytics.totalPools }}</div>
+                                                    <div class="text-base text-slate-500 mt-1">Total Pools</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+                                            <div class="report-box zoom-in">
+                                                <div class="box p-5">
+                                                    <div class="flex">
+                                                        <i data-lucide="credit-card" class="report-box__icon text-pending"></i> 
+                                                        <div class="ml-auto">
+                                                            <Icon name="healthicons:i-exam-qualification-outline"
+                                                                class=" w-16 h-10  my-2 text-primary ">
+                                                            </Icon>
+                                                         </div>
+                                                    </div>
+                                                    <div class="text-3xl font-medium leading-8 mt-6">{{ analytics.totalExams}}</div>
+                                                    <div class="text-base text-slate-500 mt-1">Total Exams</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+                                            <div class="report-box zoom-in">
+                                                <div class="box p-5">
+                                                    <div class="flex">
+                                                        <i data-lucide="monitor" class="report-box__icon text-warning"></i> 
+                                                        <div class="ml-auto">
+                                                          
+                                                            <Icon name=" mdi-account-group-outline"
+                                                                class=" w-16 h-10  my-2 text-primary ">
+                                                            </Icon>
+                                                           </div>
+                                                    </div>
+                                                    <div class="text-3xl font-medium leading-8 mt-6">10</div>
+                                                    <div class="text-base text-slate-500 mt-1">Total Contributors</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+                                            <div class="report-box zoom-in">
+                                                <div class="box p-5">
+                                                    <div class="flex">
+                                                        <i data-lucide="user" class="report-box__icon text-success"></i> 
+                                                        <div class="ml-auto">
+                                                            <div class="report-box__indicator bg-success tooltip cursor-pointer" title="22% Higher than last month"> 22% <i data-lucide="chevron-up" class="w-4 h-4 ml-0.5"></i> </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="text-3xl font-medium leading-8 mt-6">5</div>
+                                                    <div class="text-base text-slate-500 mt-1">Total Exam Groups</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                         </div>
+                        <!-- END: General Report -->
+                       <!-- BEGIN: General Report -->
+                       <div class="col-span-12 mt-8">
+                                    <div class="intro-y flex items-center h-10">
+                                        <h2 class="text-lg font-medium truncate mr-5">
+                                            Distribution
+                                        </h2>
+                                        <a href="" class="ml-auto flex items-center text-primary"> <i data-lucide="refresh-ccw" class="w-4 h-4 mr-3"></i> Reload Data </a>
+                                    </div>
+                                    <div class="grid grid-cols-12 gap-6 mt-5">
+                                        
+                                       
+                                        <div class="col-span-12 col-span-6  intro-y">
+                                            <div class=" zoom-in">
+                                                <a :href="`admin/examGroups`">
+                                                    <div class="bg-white rounded-lg shadow-lg p-6 mt-5 hover:bg-gray-50 ">
 
-                        <a :href="`admin/examGroups`">
-                            <div class="bg-white rounded-lg shadow-lg p-6 mt-5 hover:bg-gray-50 ">
+                                                        <div class="mt-6 h-80">
+                                                            <Bar :data="analytics.examGroupDistribution.data"
+                                                                :options="analytics.examGroupDistribution.options" />
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class="col-span-12 col-span-6 intro-y">
+                                            <div class=" zoom-in">
+                                                <a :href="`admin/contributors`" class="mt-4">
+                                            <div class="bg-white rounded-lg shadow-lg p-6 mt-2 hover:bg-gray-50 h-96">
+                                                <Doughnut :data="analytics.contributorDistribution.data"
+                                                    :options="analytics.contributorDistribution.options" />
+                                            </div>
+                                        </a>
 
-                                <div class="mt-6 h-80">
-                                    <Bar :data="analytics.examGroupDistribution.data"
-                                        :options="analytics.examGroupDistribution.options" />
-                                </div>
-                            </div>
-                        </a>
-
-                    </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                        </div>
+                        <!-- END: General Report -->
+                      
+                </div>
+                </div>
                 </div>
 
             </div>
